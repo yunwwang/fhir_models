0072541f238353766a10af180015152ed8f16b8c
Reece Adamson <radamson@mitre.org>
2019-08-12 15:46:02 -0400

    breaking up retrieval...

--- lib/fhir_models/validation/retrieval.rb
+++ lib/fhir_models/validation/retrieval.rb
@@ -52,15 +52,84 @@ module FHIR
           children = {}
           path_resource_map.each do |path, element|
             elements = retrieve_from_structure(sub_path, element)
-            if elements.respond_to? :each_with_index
-              elements.each_with_index do |indexed_element_value, indexed_element_path|
-                children["#{path}.#{sub_path}[#{indexed_element_path}]"] = indexed_element_value unless blank?(indexed_element_value)
-              end
-            else
-              children["#{path}.#{sub_path}"] = elements unless blank?(elements)
+            children["#{path}.#{sub_path}"] = elements unless blank?(elements)
+          end
+          index_elements(children)
+        end
+      end
+
+      # Indexes the elements in the element map
+      # @param element_map [Hash] the map of paths to element
+      # @return [Hash]
+      def self.index_elements(element_map)
+        indexed_elements = {}
+        element_map.each do |path, elements|
+          if elements.respond_to? :each_with_index
+            elements.each_with_index do |indexed_element_value, indexed_element_path|
+              indexed_elements["#{path}[#{indexed_element_path}]"] = indexed_element_value unless blank?(indexed_element_value)
+            end
+          else
+            indexed_elements[path.to_s] = elements unless blank?(elements)
+          end
+        end
+        indexed_elements
+      end
+
+      # Normalize type choice elements to [x]
+      #
+      # @param element_map [Hash]
+      # @param element_definition [FHIR::ElementDefinition]
+      # @return [Hash]
+      def self.normalize_elements(element_map, element_definition)
+        choice_type_elements = {}
+        element_map.each do |element_path, element|
+          renorm = element_path.rpartition('.').first
+          normalized_path = "#{renorm}.#{element_definition.path.split('.').last}"
+          choice_type_elements[normalized_path] ||= []
+          choice_type_elements[normalized_path].push(element).compact!
+        end
+        choice_type_elements
+      end
+
+      # Retrieve elements which have a choice of type
+      # @param resource [FHIR::Model]
+      # @param element_definition [FHIR::ElementDefinition]
+      # @param indexed [Boolean] If the elements should be returned individually or as a collection
+      # @param normalized [Boolean] If the elements with a choice of type should be normalized
+      # @return [Hash]
+      def self.retrieve_type_choice_elements(resource, element_definition, indexed: false, normalized: false)
+        elements = {}
+        element_definition.type_paths.each do |type_path|
+          type_element = retrieve_by_fhirpath(type_path, resource, indexed)
+          elements.merge!(type_element) unless blank?(type_element)
+        end
+
+        elements = normalize_elements(elements, element_definition) if normalized
+        elements
+      end
+
+      # Retrieve only the matching slices from the elements
+      # @param element_definition [FHIR::ElementDefinition]
+      # @param indexed [Boolean] If the elements should be returned individually or as a collection
+      # @return [Hash]
+      def self.handle_slices(element_map, element_definition, indexed: false)
+        # Grab Extension slices
+        return {} unless element_definition.type.first.code == 'Extension'
+
+        # Only select the elements which match the slice profile.
+        if indexed
+          # Elements are already indexed
+          element_map.select! do |_k, element|
+            element.url == element_definition.type.first.profile.first
+          end
+        else
+          sliced_elements = {}
+          element_map.each do |element_path, element|
+            sliced_elements[element_path] = element.select do |extension|
+              extension.url == element_definition.type.first.profile.first
             end
           end
-          children
+          sliced_elements
         end
       end
 
@@ -74,50 +143,13 @@ module FHIR
         # Check if we were provided a path that includes extensions (like in the ElementDefinition id versus the path)
         path = element_definition.path
 
-        # Check for multiple choice types
-        if element_definition.choice_type?
-          elements = {}
-          element_definition.type.each do |type|
-            choice_type = type.code[0].upcase + type.code[1..-1]
-            type_element = retrieve_by_fhirpath(path.gsub('[x]', choice_type), resource, indexed)
-            elements.merge!(type_element) unless blank?(type_element)
-          end
-          if normalized
-            choice_type_elements = {}
-            elements.each do |element_path, element|
-              renorm = element_path.rpartition('.').first
-              normalized_path = "#{renorm}.#{element_definition.path.split('.').last}"
-              choice_type_elements[normalized_path] ||= []
-              choice_type_elements[normalized_path].push(element).compact!
-            end
-            elements = choice_type_elements
-          end
-        else
-          elements = retrieve_by_fhirpath(path, resource, indexed)
-        end
+        elements = if element_definition.choice_type?
+                     retrieve_type_choice_elements(resource, element_definition, indexed: indexed, normalized: normalized)
+                   else
+                     retrieve_by_fhirpath(path, resource, indexed)
+                   end
 
-        # Handle Slices
-        if element_definition.sliceName
-          # TODO: Need to be able to get other types of slices
-          # Grab Extension slices
-          return {} unless element_definition.type.first.code == 'Extension'
-
-          # Only select the elements which match the slice profile.
-          if indexed
-            # Elements are already indexed
-            elements.select! do |_k, element|
-              element.url == element_definition.type.first.profile.first
-            end
-          else
-            sliced_elements = {}
-            elements.each do |element_path, element|
-              sliced_elements[element_path] = element.select do |extension|
-                extension.url == element_definition.type.first.profile.first
-              end
-            end
-            elements = sliced_elements
-          end
-        end
+        elements = handle_slices(elements, element_definition, indexed: indexed, normalized: normalized) if element_definition.sliceName
         elements
       end
 
@@ -125,7 +157,8 @@ module FHIR
         obj.respond_to?(:empty?) ? obj.empty? : obj.nil?
       end
 
-      private_class_method :blank?, :path_parts, :retrieve_from_structure, :indexed_elements_by_fhirpath
+      private_class_method :blank?, :path_parts, :retrieve_from_structure, :indexed_elements_by_fhirpath,
+                           :index_elements, :normalize_elements, :retrieve_type_choice_elements, :handle_slices
     end
   end
 end
