15ff1e991d2cb046e62b382e1912496b1bc69a23
Reece Adamson <radamson@mitre.org>
2019-08-13 13:33:23 -0400

    remove unnecessary spec

--- spec/lib/fhir_models/bootstrap/my_example_spec.rb
+++
@@ -1,7 +0,0 @@
-describe 'FHIR::JSON' do
-
-  it 'allows FHIR::Models to be represented as JSON' do
-    patient = FHIR::Patient.new
-    expect(patient.to_json).to eq("{\n  \"resourceType\": \"Patient\"\n}")
-  end
-end
