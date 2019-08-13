7ce7156cad45bb573f898b7ee069a3b3b318fa80
Reece Adamson <radamson@mitre.org>
2019-08-13 17:49:06 -0400

    clean up cardinality validator tests

--- spec/unit/validation/element_validator/cardinality_validator_spec.rb
+++ spec/unit/validation/element_validator/cardinality_validator_spec.rb
@@ -1,28 +1,16 @@
 describe FHIR::Validation::CardinalityValidator do
-
-  let(:validator) { FHIR::Validation::CardinalityValidator }
+  let(:validator) { described_class }
 
   let(:resource) do
     FHIR::Patient.new
   end
-  
-  shared_context 'a single element' do
-    let(:id) {'the id'}
-  end
-
-  shared_context 'multiple elements' do
-    let(:id) { [1,2] }
-  end
-
-  shared_context 'no elements' do
-    let(:id) { nil }
-  end
 
   describe '#validate' do
-    let(:element_definition) do  FHIR::ElementDefinition.new(min: 0,
-                                                             max: 1,
-                                                             id: 'Patient',
-                                                             path: 'Patient')
+    let(:element_definition) do
+      FHIR::ElementDefinition.new(min: 0,
+                                  max: 1,
+                                  id: 'Patient',
+                                  path: 'Patient')
     end
 
     it 'skips cardinality on the root element' do
@@ -30,28 +18,47 @@ describe FHIR::Validation::CardinalityValidator do
       expect(results).to be_nil
     end
 
-    scenarios = {'0..1' => {'a single element' => :pass, 'no elements' => :pass, 'multiple elements' => :fail},
-                 '0..*' => {'a single element' => :pass, 'no elements' => :pass, 'multiple elements' => :pass},
-                 '1..1' => {'a single element' => :pass, 'no elements' => :fail, 'multiple elements' => :fail},
-                 '1..*' => {'a single element' => :pass, 'no elements' => :fail, 'multiple elements' => :pass}}
+    scenarios = { '0..1' => { 'with a single element' => :pass, 'with no elements' => :pass, 'with multiple elements' => :fail },
+                  '0..*' => { 'with a single element' => :pass, 'with no elements' => :pass, 'with multiple elements' => :pass },
+                  '1..1' => { 'with a single element' => :pass, 'with no elements' => :fail, 'with multiple elements' => :fail },
+                  '1..*' => { 'with a single element' => :pass, 'with no elements' => :fail, 'with multiple elements' => :pass } }
 
     scenarios.each do |cardinality, number_of_elements_scenario|
-      context "when the cardinality is #{cardinality[0]}..#{cardinality[-1]}" do
-        let(:element_definition) do  FHIR::ElementDefinition.new(min: cardinality[0],
-                                                                 max: cardinality[-1],
-                                                                 id: 'Patient.id',
-                                                                 path: 'Patient.id')
-        end
-        number_of_elements_scenario.each do |number_of_elements, expected_result|
-          context "when the element contains #{number_of_elements}" do
-            include_context number_of_elements
-            it "#{expected_result} when the element contains #{number_of_elements}" do
-              resource.id = id
-              results = validator.validate(resource, element_definition)
-              expect(results.size).to eq(1)
-              expect(results).to all(have_attributes(validation_type: :cardinality))
-              expect(results).to all(have_attributes(result: expected_result))
+      number_of_elements_scenario.each do |number_of_elements, expected_result|
+        context "when the cardinality is #{cardinality[0]}..#{cardinality[-1]} and the element contains #{number_of_elements}" do
+          let(:element_definition) do
+            FHIR::ElementDefinition.new(min: cardinality[0],
+                                        max: cardinality[-1],
+                                        id: 'Patient.id',
+                                        path: 'Patient.id')
+          end
+
+          let(:id) do
+            case number_of_elements
+            when 'with a single element'
+              'the id'
+            when 'with no elements'
+              nil
+            when 'with multiple elements'
+              [1, 2]
             end
+          end
+
+          let(:results) do
+            resource.id = id
+            validator.validate(resource, element_definition)
+          end
+
+          it 'includes one result' do
+            expect(results.size).to eq(1)
+          end
+
+          it 'all results have a validation_type of cardinality' do
+            expect(results).to all(have_attributes(validation_type: :cardinality))
+          end
+
+          it "the result status is #{expected_result}" do
+            expect(results).to all(have_attributes(result: expected_result))
           end
         end
       end
