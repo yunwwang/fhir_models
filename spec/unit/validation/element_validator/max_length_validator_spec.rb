3bb46efd329f1efefaf0fffa53936a6cf90ed278
Reece Adamson <radamson@mitre.org>
2019-08-13 18:39:44 -0400

    cleanup max length validator spec

--- spec/unit/validation/element_validator/max_length_validator_spec.rb
+++ spec/unit/validation/element_validator/max_length_validator_spec.rb
@@ -1,49 +1,55 @@
 describe FHIR::Validation::MaxLengthValidator do
-  let(:validator) {FHIR::Validation::MaxLengthValidator}
-  let(:restricted_string) {'foo'}
+  let(:validator) { described_class }
+  let(:restricted_string) { 'foo' }
   let(:resource) { FHIR::Patient.new(gender: restricted_string) }
-  let(:element_definition) {FHIR::ElementDefinition.new(id: 'Patient.gender', path: 'Patient.gender')}
+  let(:element_definition) { FHIR::ElementDefinition.new(id: 'Patient.gender', path: 'Patient.gender') }
+
+  shared_examples 'max length results' do
+    it 'results have validation type :max_length' do
+      expect(results).to all(have_attributes(validation_type: :max_length))
+    end
+  end
+
+  shared_examples 'returns one result' do
+    it 'returns one result' do
+      expect(results.size).to eq(1)
+    end
+  end
+
+  shared_context 'with with results for max length' do |max_length|
+    let(:results) do
+      element_definition.maxLength = max_length
+      validator.validate(resource, element_definition)
+    end
+  end
 
   context 'when the string is shorter than the maxLength' do
-    before do
-      element_definition.maxLength = 10
-      @results = validator.validate(resource, element_definition)
-    end
+    include_context 'with with results for max length', 10
 
-    it 'returns one result' do
-      expect(@results.size).to eq(1)
-    end
+    include_examples 'returns one result'
+    include_examples 'max length results'
 
     it 'passes' do
-      expect(@results.first).to have_attributes(validation_type: :max_length)
-      expect(@results.first).to have_attributes(result: :pass)
+      expect(results).to all(have_attributes(result: :pass))
     end
   end
 
   context 'when the string is longer than the maxLength' do
-    before do
-      element_definition.maxLength = 2
-      @results = validator.validate(resource, element_definition)
-    end
+    include_context 'with with results for max length', 2
 
-    it 'returns one result' do
-      expect(@results.size).to eq(1)
-    end
+    include_examples 'returns one result'
+    include_examples 'max length results'
 
     it 'fails' do
-      expect(@results.first).to have_attributes(validation_type: :max_length)
-      expect(@results.first).to have_attributes(result: :fail)
+      expect(results).to all(have_attributes(result: :fail))
     end
   end
 
   context 'when no max length is specified' do
-    before do
-      element_definition.maxLength = nil
-      @results = validator.validate(resource, element_definition)
-    end
+    include_context 'with with results for max length', nil
 
     it 'returns no results' do
-      expect(@results).to be_nil
+      expect(results).to be_nil
     end
   end
 end
