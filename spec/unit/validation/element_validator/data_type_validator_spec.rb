c4cb6b2c150c3361af943c16936b77b46164089f
Reece Adamson <radamson@mitre.org>
2019-08-13 18:03:38 -0400

    clean up data type validator test

--- spec/unit/validation/element_validator/data_type_validator_spec.rb
+++ spec/unit/validation/element_validator/data_type_validator_spec.rb
@@ -1,70 +1,113 @@
 describe FHIR::Validation::DataTypeValidator do
-
-  let(:validator) { FHIR::Validation::DataTypeValidator }
+  let(:validator) { described_class }
 
   let(:resource) do
     FHIR::Patient.new(id: 2,
-                      name: {given: 'Bob'},
-                      communication: [{language: 'English'}, {language: 'Spanish'}],
+                      name: { given: 'Bob' },
+                      communication: [{ language: 'English' }, { language: 'Spanish' }],
                       deceasedBoolean: false,
                       deceasedDateTime: 'YYYY-MM-DD')
+  end
+
+  shared_context 'with results' do
+    let(:results) { validator.validate(resource, element_definition) }
+  end
+
+  shared_examples 'data type result' do
+    it 'returns at least one result' do
+      expect(results).not_to be_empty
+    end
+
+    it 'returns no failing results' do
+      expect(results.select { |res| res.result == :fail }).to be_empty
+    end
+  end
+
+  shared_examples 'cardinality results' do
+    it 'returns cardinality results' do
+      expect(results.select { |res| res.validation_type == :cardinality }).not_to be_empty
+    end
   end
 
   describe '#validate' do
     let(:element_definition) do
       FHIR::ElementDefinition.new(id: 'Patient',
                                   path: 'Patient',
-                                  type: {code: 'Patient'})
+                                  type: { code: 'Patient' })
     end
+
+    include_context 'results'
+
     it 'skips the root element' do
       results = validator.validate(resource, element_definition)
       expect(results).to be_nil
     end
-    context 'with an element with a single type' do
+
+    context 'with a valid element with a single type' do
       let(:element_definition) do
         FHIR::ElementDefinition.new(id: 'Patient.name',
                                     path: 'Patient.name',
-                                    type: {code: 'HumanName'})
+                                    type: { code: 'HumanName' })
       end
-      it 'returns an array of results for the single type' do
-        results = validator.validate(resource, element_definition)
-        expect(results).to_not be_empty
-        expect(results.select {|res| res.result == :fail}).to be_empty
-        expect(results.select {|res| res.validation_type == :cardinality}).to_not be_empty
+
+      include_examples 'data type result'
+      include_examples 'cardinality results'
+    end
+
+    context 'when the type of the element is unknown' do
+      let(:element_definition) do
+        FHIR::ElementDefinition.new(id: 'Patient.name',
+                                    path: 'Patient.name',
+                                    type: { code: 'Foo' })
       end
-      it 'Returns a warning if the type is unknown' do
-        element_definition.type.first.code = 'Foo'
-        results = validator.validate(resource, element_definition)
+
+      include_context 'results'
+
+      include_examples 'data type result'
+
+      it 'returns results with warnings' do
         expect(results).to all(have_attributes(result: :warn))
-        expect(results.first).to have_attributes(text: "Unknown type: Foo")
+      end
+
+      it 'lists the unknown type' do
+        expect(results.first).to have_attributes(text: 'Unknown type: Foo')
       end
     end
+
     context 'with an element that has a choice of types' do
       let(:resource) do
-        obs = FHIR::Observation.new(valueQuantity: {value: 2, code: 'mm'})
-        obs.valueCodeableConcept = FHIR::CodeableConcept.new(coding: {code: 'foo', system: 'bar'}, text: 'fake')
+        obs = FHIR::Observation.new(valueQuantity: { value: 2, code: 'mm' })
+        obs.valueCodeableConcept = FHIR::CodeableConcept.new(coding: { code: 'foo', system: 'bar' }, text: 'fake')
         obs
       end
+
       let(:element_definition) do
         FHIR::ElementDefinition.new(id: 'Observation.value[x]',
                                     path: 'Observation.value[x]',
                                     type: types)
       end
 
-      let(:type1) { {code: 'Quantity'} }
+      let(:results) { validator.validate(resource, element_definition) }
 
-      let(:type2) { {code: 'CodeableConcept'} }
+      let(:type1) { { code: 'Quantity' } }
+
+      let(:type2) { { code: 'CodeableConcept' } }
 
       let(:types) do
         [type1, type2]
       end
-      it 'returns an array of results for elements with a choice of type' do
-        results = validator.validate(resource, element_definition)
-        expect(results).to_not be_empty
+
+      include_context 'results'
+
+      include_examples 'data type result'
+      include_examples 'cardinality results'
+
+      it 'includes results for the first type' do
         expect(results).to include(have_attributes(element_path: a_string_including(element_definition.type[0].code)))
+      end
+
+      it 'includes results for the second type' do
         expect(results).to include(have_attributes(element_path: a_string_including(element_definition.type[1].code)))
-        expect(results.select {|res| res.result == :fail}).to be_empty
-        expect(results.select {|res| res.validation_type == :cardinality}).to_not be_empty
       end
     end
   end
