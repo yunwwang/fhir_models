1f9f4ca6ba31cf702d31012e9e32f3b5c2dcb4ec
Reece Adamson <radamson@mitre.org>
2019-08-14 08:21:23 -0400

    cleanup structure validator test

--- spec/unit/validation/structure_validator_spec.rb
+++ spec/unit/validation/structure_validator_spec.rb
@@ -1,12 +1,6 @@
 describe FHIR::Validation::StructureValidator do
-  FIXTURES_DIR = File.join('test', 'fixtures')
-
-  # let(:profile) {FHIR::StructureDefinition.new}
-  # let(:validator) do
-  #   FHIR::Validation::StructureValidator.new(profile)
-  # end
-  #
   let(:profile) { FHIR::StructureDefinition.new(snapshot: { element: elements }) }
+
   let(:elements) do
     [FHIR::ElementDefinition.new(id: 'Patient',
                                  path: 'Patient',
@@ -31,16 +25,14 @@ describe FHIR::Validation::StructureValidator do
   end
 
   let(:validator) do
-    FHIR::Validation::StructureValidator.new(profile)
+    described_class.new(profile)
   end
 
   context 'with the default validators' do
-    before(:example) do
-      @results = validator.validate(resource)
-    end
+    let(:results) { validator.validate(resource) }
+
     it 'returns cardinality results' do
-      cardinality_results = @results.select { |res| res.validation_type == :cardinality }
-      expect(cardinality_results.length.positive?).to be_truthy
+      expect(results.select { |res| res.validation_type == :cardinality }).not_to be_empty
     end
   end
 end
